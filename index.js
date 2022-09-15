e692d0d0586cc22c9fd44049bc8a49c5040a2111
vasylcode <vasylcode@gmail.com>
2022-09-15 16:58:19 +0200

    info router

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const PORT = 5050;
+const PORT = process.env.PORT || 5050;
 
 app.get('/', (req, res) => {
 	res.send('F5Checker!');
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
 
 async function start() {
 	try {
-		app.listen(PORT, () => console.log('F5Checker started, port ' + PORT));
+		app.listen(PORT, () => console.log('F5Checker started on port ' + PORT));
 	} catch (error) {
 		console.log('Server Error:', error.message);
 		process.exit(1);
@@ -17,3 +17,5 @@ async function start() {
 }
 
 start();
+
+app.use('/info', require('./routes/info.routes'));
---
+++ routes/info.routes.js
@@ -0,0 +1,52 @@
+const { Router, json } = require('express');
+const router = Router();
+
+const os = require('os');
+const checkDiskSpace = require('check-disk-space').default;
+
+function formatSeconds(seconds) {
+	seconds = Number(seconds);
+	let d = Math.floor(seconds / (3600*24));
+	let h = Math.floor(seconds % (3600*24) / 3600);
+	let m = Math.floor(seconds % 3600 / 60);
+	// let s = Math.floor(seconds % 60);
+	
+	let dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
+	let hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+	let mDisplay = m > 0 ? m + (m == 1 ? " minute " : " minutes ") : "";
+	// let sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+	return dDisplay + hDisplay + mDisplay;
+}
+
+
+function formatBytes(bytes, decimals = 2) {
+    if (!+bytes) return '0 Bytes'
+
+    const k = 1024
+    const dm = decimals < 0 ? 0 : decimals
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k))
+
+    return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
+}
+
+router.get('/', async (req, res) => {
+	let data = {
+		"host": os.hostname() +" – "+ os.platform(),
+		"uptime": formatSeconds(os.uptime),
+		"cpu": os.cpus().length,
+		"loadavg": os.loadavg(),
+		"memory": formatBytes(os.totalmem() - os.freemem()) +" / "+ formatBytes(os.totalmem())
+	};
+
+	await checkDiskSpace('/').then((disk) => {
+		disk.free = formatBytes(disk.free);
+		disk.size = formatBytes(disk.size);
+		data.disk = disk;
+	});
+	
+	res.status(200).send(data);
+});
+
+module.exports = router;
