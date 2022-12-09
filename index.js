1496e78670180ad4a93e3576b8cc5bfbac1d41d0
vasylcode <vasylcode@gmail.com>
2022-12-09 22:41:48 +0200

    simple to route added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5050;
+app.use(cors());
 
 app.get('/', (req, res) => {
 	res.send('F5Checker!');
