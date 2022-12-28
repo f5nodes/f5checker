ae7f665606303abede7571fbd5895970297e07bb
ezvuzdetskyi <yehor.zvuzdetskyi@gmail.com>
2022-12-28 15:23:46 +0200

    use httpStatuses instead of pure numbers

--- src/routes/info.ts
+++ src/routes/info.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from 'express';
 import { getServerInfo } from '../service/info';
 import { check } from '../service/checker';
 import { NodeNames } from '../service/checker/enums';
+import httpStatus from 'http-status';
 
 const router = Router();
 
@@ -12,19 +13,19 @@ router.get('/info/server', async (req: Request, res: Response) => {
 router.get('/info/check-node', async (req: Request<unknown, unknown, unknown, {nodeName?: NodeNames, address?: string}>, res: Response) => {
 	const {nodeName, address} = req.query
 	if (!nodeName) {
-		return res.status(400).send({
+		return res.status(httpStatus.BAD_REQUEST).send({
 			message: 'nodeName should be defined'
 		})
 	}
 	
 	if (!address) {
-		return res.status(400).send({
+		return res.status(httpStatus.BAD_REQUEST).send({
 			message: 'address should be defined'
 		})
 	}
 	
 	if (!(nodeName in NodeNames)) {
-		return res.status(400).send({
+		return res.status(httpStatus.BAD_REQUEST).send({
 			message: `Can not find node for ${nodeName}`
 		})
 	}
