f7d47703aec94e34963289ca08a862fc1415be92
vasylcode <vasylcode@gmail.com>
2023-01-08 18:21:05 +0100

    celestia console log deleted

--- index.ts
+++ index.ts
@@ -1,15 +1,14 @@
 import express, { NextFunction, Request, Response } from 'express';
-import cors from 'cors'
-import routes from './src/routes'
+import cors from 'cors';
+import routes from './src/routes';
 import { normalizeError } from './src/errors/normalizeError';
 import httpStatus from 'http-status';
-import celestiaChecker from './src/services/celestia'
-const app = express()
+const app = express();
 
 const PORT = process.env.PORT || 5050;
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
 	res.send('F5Checker!');
@@ -18,7 +17,6 @@ app.get('/', (req, res) => {
 async function start() {
 	try {
 		app.listen(PORT, () => console.log('F5Checker started on port ' + PORT));
-		console.log(await celestiaChecker.getInfo());
 	} catch (error: any) {
 		console.log('Server Error:', error.message);
 		process.exit(1);
@@ -30,8 +28,8 @@ app.use('/api', routes);
 
 // always should be last
 app.use((err: any, req: any, res: any, next: any) => {
-	const error = normalizeError(err)
+	const error = normalizeError(err);
 	res.status(error.status || httpStatus.INTERNAL_SERVER_ERROR).send({
 		message: error.message,
-	})
-})
+	});
+});
