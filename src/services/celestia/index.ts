9d237157c9fcf505e4c2c3cb901c66bc00d69f43
ezvuzdetskyi <yehor.zvuzdetskyi@gmail.com>
2023-01-08 11:54:11 +0200

    celestia check

--- src/services/celestia/index.ts
+++ src/services/celestia/index.ts
@@ -1,15 +1,34 @@
 import { Checker } from '../checker/interface';
 import { get } from '../../api';
+import { verifyResponse } from '../../utils/verifyResponse';
+
+interface CelestiaInfoResponse {
+  result: {
+    node_info: {
+      sync_info: {
+        latest_block_height: string,
+        earliest_block_height: string,
+        catching_up: boolean
+      },
+    }
+  };
+}
 
 class CelestiaChecker implements Checker {
   async getLogs(): Promise<any> {
-    
+
   }
 
   async getInfo(): Promise<any> {
-    const celestaiStatus = await get('http://localhost:26657/status');
-    console.log(celestaiStatus);
+    const celestaiStatus = await get<CelestiaInfoResponse>('http://localhost:26657/status');
+    verifyResponse(celestaiStatus);
+    const {data: {result: {node_info: {sync_info:syncInfo}}}} = celestaiStatus
+    return {
+      latestBlockHeight: syncInfo.latest_block_height,
+      earliestBlockHeight: syncInfo.earliest_block_height,
+      catchingUp: syncInfo.catching_up,
+    }
   }
 }
 
-export default new CelestiaChecker()
+export default new CelestiaChecker();
