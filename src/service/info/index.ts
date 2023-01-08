15b7793363002cf06361bb23847ec388e8ef6cfd
vasylcode <52459025+vasylcode@users.noreply.github.com>
2023-01-08 13:36:34 +0200

    Simple route (#6)
    * add simple route & space free
    
    * update simple route
    
    * ^ removed

--- src/service/info/index.ts
+++ src/service/info/index.ts
@@ -1,45 +1,74 @@
 import os from 'os';
-import { ServerInfo, Space } from './interfaces';
+import axios from 'axios';
+import { SimpleInfo, ServerInfo, Space } from './interfaces';
 import checkDiskSpace from 'check-disk-space';
 import { formatBytes } from './utils';
 
+const getLocation = async (): Promise<string> => {
+	const {
+		data: { countryName },
+	} = await axios.get('https://freeipapi.com/api/json');
+	return countryName;
+};
+
 const getLoadAvg = (): number[] => {
-  const coresCount = os.cpus().length
-  return os.loadavg().map(item => Number(((item / coresCount) * 100).toFixed(2)))
-}
+	const coresCount = os.cpus().length;
+	return os.loadavg().map((item) => Number(((item / coresCount) * 100).toFixed(2)));
+};
 
-const getRam = (): Space => {
-  const totalRAM = os.totalmem();
-  const freeRAM = os.freemem();
-  
-  return {
-    total: formatBytes(totalRAM),
-    used: formatBytes(totalRAM-freeRAM)
-  }
-}
+const getRam = (decimals = 2): Space => {
+	const totalRAM = os.totalmem();
+	const freeRAM = os.freemem();
 
-const getDisk = async (): Promise<Space> => {
-  const disk = await checkDiskSpace('/')
-  return {
-    total: formatBytes(disk.size),
-    used: formatBytes(disk.size - disk.free)
-  }
-}
+	return {
+		total: formatBytes(totalRAM, decimals),
+		used: formatBytes(totalRAM - freeRAM, decimals),
+		free: formatBytes(freeRAM, decimals),
+	};
+};
+
+const getRamTotal = async (): Promise<string> => {
+	return (await getRam(0)).total;
+};
+
+const getDisk = async (decimals = 2): Promise<Space> => {
+	const disk = await checkDiskSpace('/');
+	return {
+		total: formatBytes(disk.size, decimals),
+		used: formatBytes(disk.size - disk.free, decimals),
+		free: formatBytes(disk.free, decimals),
+	};
+};
+
+const getDiskTotal = async (): Promise<string> => {
+	return (await getDisk(0)).total;
+};
 
 const getUptime = (): string => {
-  const date = new Date(os.uptime() * 1000)
-  const days = date.getDay()
-  const hoursAndSeconds = date.toISOString().slice(11, 16).split(':')
-  return `${days}d ${hoursAndSeconds[0]}h ${hoursAndSeconds[1]}m`
+	const date = new Date(os.uptime() * 1000);
+	const days = date.getDay();
+	const hoursAndSeconds = date.toISOString().slice(11, 16).split(':');
+	return `${days}d ${hoursAndSeconds[0]}h ${hoursAndSeconds[1]}m`;
+};
+
+export async function getServerSimpleInfo(): Promise<SimpleInfo> {
+	return {
+		hostname: os.hostname(),
+		cpu: os.cpus().length,
+		ram: await getRamTotal(),
+		disk: await getDiskTotal(),
+		location: await getLocation(),
+	};
 }
 
 export async function getServerInfo(): Promise<ServerInfo> {
-  return {
-    hostname: os.hostname(),
-    uptime: getUptime(),
-    loadavg: getLoadAvg(),
-    ram: getRam(),
-    cpu: os.cpus().length,
-    disk: await getDisk()
-  };
+	return {
+		hostname: os.hostname(),
+		uptime: getUptime(),
+		loadavg: getLoadAvg(),
+		ram: getRam(),
+		cpu: os.cpus().length,
+		disk: await getDisk(),
+		location: await getLocation(),
+	};
 }
--- src/service/info/interfaces.ts
+++ src/service/info/interfaces.ts
@@ -1,13 +1,23 @@
+export interface SimpleInfo {
+	hostname: string;
+	cpu: number;
+	ram: string;
+	disk: string;
+	location: string;
+}
+
 export interface ServerInfo {
-  hostname: string,
-  cpu: number,
-  uptime: string,
-  ram: Space
-  loadavg: number[],
-  disk: Space
+	hostname: string;
+	cpu: number;
+	uptime: string;
+	ram: Space;
+	loadavg: number[];
+	disk: Space;
+	location: string;
 }
 
 export interface Space {
-  used: string,
-  total: string
+	used: string;
+	total: string;
+	free: string;
 }
