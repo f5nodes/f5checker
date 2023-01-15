6a5d61c19c38f7fced9bc9cde977b349b5a1ff4a
vasylcode <vasylcode@gmail.com>
2023-01-16 00:25:01 +0100

    massa active rolls fixed

--- src/services/massa/index.ts
+++ src/services/massa/index.ts
@@ -6,46 +6,43 @@ import { NodeInfoPayload } from '../../routes/info/interfaces';
 import { validationError } from '../../errors';
 
 class MassaChecker implements Checker {
-  async getLogs(): Promise<any> {
-    
-  }
+	async getLogs(): Promise<any> {}
 
-  async getInfo(params: NodeInfoPayload): Promise<any> {
-    console.log(params);
-    this.validate(params)
-    const {address} = params
-    const massaResponse = await post<MassaResponse>('https://test.massa.net/api/v2', {
-      jsonrpc: "2.0",
-      id: 1,
-      method: "get_addresses",
-      params: [[address]]
-    })
+	async getInfo(params: NodeInfoPayload): Promise<any> {
+		this.validate(params);
+		const { address } = params;
+		const massaResponse = await post<MassaResponse>('https://test.massa.net/api/v2', {
+			jsonrpc: '2.0',
+			id: 1,
+			method: 'get_addresses',
+			params: [[address]],
+		});
 
-    verifyResponse(massaResponse);
-    
-    const {data: {result}} = massaResponse
+		verifyResponse(massaResponse);
 
-    const massaInfo = result[0]
+		const {
+			data: { result },
+		} = massaResponse;
 
-    const response = {
-      address: massaInfo.address,
-      finalBalance: massaInfo.final_balance,
-      finalRollCount: massaInfo.final_roll_count,
-      activeRolls: 0,
-    }
+		const massaInfo = result[0];
 
-    massaInfo.cycle_infos.forEach(({active_rolls}) => {
-      response.activeRolls += active_rolls
-    })
+		const response = {
+			address: massaInfo.address,
+			finalBalance: massaInfo.final_balance,
+			finalRollCount: massaInfo.final_roll_count,
+			activeRolls: 0,
+		};
 
-    return response;
-  }
-  
-  validate(params: NodeInfoPayload): asserts params is MassaInfoParams {
-    if (!('address' in params)) {
-      throw validationError('Address should be defined for massa node')
-    }
-  } 
+		response.activeRolls = massaInfo.cycle_infos[massaInfo.cycle_infos.length - 1].active_rolls;
+
+		return response;
+	}
+
+	validate(params: NodeInfoPayload): asserts params is MassaInfoParams {
+		if (!('address' in params)) {
+			throw validationError('Address should be defined for massa node');
+		}
+	}
 }
 
-export default new MassaChecker()
+export default new MassaChecker();
