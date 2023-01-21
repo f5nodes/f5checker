cada684291383fc357ee779acd2aa2bd16882783
vasylcode <vasylcode@gmail.com>
2023-01-21 12:43:40 +0100

    setup.sh added

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,10 +1,1 @@
 #!/bin/bash
-
-. <(wget -qO- https://raw.githubusercontent.com/f5nodes/root/main/install/node.sh)
-
-npm install pm2 -g
-git clone https://github.com/f5nodes/f5checker.git
-cd f5checker
-npm install
-pm2 install typescript
-pm2 start index.ts --name "f5checker"
---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+. <(wget -qO- https://raw.githubusercontent.com/f5nodes/root/main/install/node.sh)
+
+npm install pm2 -g
+git clone https://github.com/f5nodes/f5checker.git
+cd f5checker
+npm install
+pm2 install typescript
+pm2 start index.ts --name "f5checker"
+pm2 log
