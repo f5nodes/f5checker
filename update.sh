5ee8b894d21c29b475eba1d36d08711fe5493921
vasylcode <vasylcode@gmail.com>
2023-01-21 13:02:52 +0100

    update

--- update.sh
+++ update.sh
@@ -9,7 +9,7 @@ latest_version=$(curl -s "https://api.f5checker.com/version")
 echo "[$(date)] Current v$current_version, latest - v$latest_version"
 
 if [ "$latest_version" != "$current_version" ]; then
-    echo "[$(date)] New version $latest_version is detected! Old version: $current_version. Updating..."
+    echo "[$(date)] New version v$latest_version is detected! Old version: v$current_version. Updating..."
     git pull
     npm install
     pm2 restart f5checker
