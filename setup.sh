6c227446eccccdd244867110800a23ec8820a9d2
vasylcode <52459025+vasylcode@users.noreply.github.com>
2023-01-21 14:04:45 +0200

    Setup & Update scripts (#9)
    * link added
    
    * setup.sh added
    
    * update script upd
    
    * scripts dir deleted
    
    * .
    
    * update

--- setup.sh
+++ setup.sh
@@ -3,8 +3,9 @@
 . <(wget -qO- https://raw.githubusercontent.com/f5nodes/root/main/install/node.sh)
 
 npm install pm2 -g
-git clone
+git clone https://github.com/f5nodes/f5checker.git
 cd f5checker
 npm install
 pm2 install typescript
 pm2 start index.ts --name "f5checker"
+pm2 log
